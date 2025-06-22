7178d503b9e5c674b7aed48724b81e6a8547350b
vadim-troian <vadymtroiyan@gmail.com>
2025-06-22 13:14:30 +0300

    Update 002.js
    fixed: added  () => to setTimeout

--- solutions/002.js
+++ solutions/002.js
@@ -1,18 +1,18 @@
 function fetchWeather() {
   return new Promise((resolve, reject) =>
-    setTimeout(resolve({ temp: 15, condition: "Облачно" }), 1.1 * 1000)
+    setTimeout(() => resolve({ temp: 15, condition: "Облачно" }), 1.1 * 1000)
   );
 }
 
 function fetchExchangeRate() {
   return new Promise((resolve, reject) =>
-    setTimeout(resolve(91.5), 0.7 * 1000)
+    setTimeout(() => resolve(91.5), 0.7 * 1000)
   );
 }
 
 function fetchTopHeadline() {
   return new Promise((resolve, reject) =>
-    setTimeout(
+    setTimeout(() =>
       resolve("Ученые обнаружили новый способ изучения асинхронности!"),
       1.4 * 1000
     )
